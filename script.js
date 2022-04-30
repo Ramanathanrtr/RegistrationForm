6743c828e23ed997f2fb7d859ddeacea486ad109
ram Tech <98199163+Ramanathanrtr@users.noreply.github.com>
2022-04-30 07:30:11 +0000

    js regular expression validation

--- script.js
+++ script.js
@@ -8,20 +8,28 @@ let date  = form.elements['datetime'];
 let address = form.elements['addresstext'];
 let country = form.elements['country']
 
-    if(fname.value=="")
-    {
-      console.log("firstname is empty")
-      window.alert("Enter firstname");
-      fname.focus();
-      return false;
-    }
-    if(lname.value=="")
-    {
-      console.log("lastname is empty");
-      window.alert("Enter lastname");
-      lname.focus();
-      return false;
-    }
+
+
+            var re = /^[ A-Za-z_@./#&+-]*$/;
+            if (!((re.test(fname.value))&&(re.test(lname.value)))) {
+                window.alert("Fname or Lname is invalid");
+                return false;
+            }
+    
+    // if(fname.value=="")
+    // {
+    //   console.log("firstname is empty")
+    //   window.alert("Enter firstname");
+    //   fname.focus();
+    //   return false;
+    // }
+    // if(lname.value=="")
+    // {
+    //   console.log("lastname is empty");
+    //   window.alert("Enter lastname");
+    //   lname.focus();
+    //   return false;
+    // }
     if(date.value=="")
     {
       console.log("date is invalid");
@@ -47,3 +55,38 @@ let country = form.elements['country']
     }
     return true;
   }
+
+function getArea()
+{
+console.log("getArea");
+  const formData = document.getElementById("form1");
+  let pincode = formData.elements['pincode'];
+  
+  
+    var request = new XMLHttpRequest();
+
+  
+request.open('GET', 'https://api.postalpincode.in/pincode/'+pincode.value, false);  // `false` makes the request synchronous
+request.send(null);
+
+if (request.status === 200) {
+  console.log(request.responseText);
+  parseJson(request.responseText)
+}
+}
+
+function parseJson(data)
+  {
+    let obj = JSON.parse(data);
+    let objData = obj[0].PostOffice[0].Name;
+    console.log("new data:"+objData);
+    document.getElementById("addresstext").innerHTML = objData;
+  }
+
+function setBackColor()
+{
+  var color = document.getElementById("color");
+  var strColor = color.value;
+  document.body.style.backgroundColor = strColor;
+  
+}
